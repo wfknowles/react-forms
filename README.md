4e4d7ea7d05c7e7bd7849fb7f5dfc4649fa1e822
Will Knowles <will@willknowles.com>
2021-12-03 18:56:35 -0600

    version 1 added

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
 # react-forms
 A form handling class for retrieving and reducing nested form data.
+https://github.com/wfknowles/react-forms
+
+## Sample Component & Library Usage
+
+```
+    import React, { useState } from 'react'
+    import ReactForms from 'ReactForm.js'
+
+    import { useAppContext } from '../GlobalState';
+    import { SOME_ACTION } from '../actions';
+
+    function formComponent() {
+        const [ state, dispatch ] = useAppContext();
+        const { stateObj } = state;
+
+        const changeHandler = (e) => {
+
+        // using ReactForms for retrieving and reducing form object
+        const inputData = ReactForms.value(e);
+        const reducedState = ReactForms.reduce({ stateObj }, inputData);
+
+        if (reducedState) {
+            dispatch({
+                type: SOME_ACTION,
+                reducedState
+            });
+        }
+
+        return (
+            <form>
+                <input name="stateObj.fruit.apple" onChange={changeHandler}/>
+                <input name="stateObj.fruit.banana" onChange={changeHandler}/>
+                <input name="stateObj.foo" onChange={changeHandler}/>
+                <input name="stateObj.bar" onChange={changeHandler}/>
+                <input name="stateObj.pizza.cheese" onChange={changeHandler}/>
+                <input name="stateObj.pizza.pepperoni" onChange={changeHandler}/>
+            </form>
+        )
+
+    }
+
+```
+
+## Sample Reducer
+```
+    case SOME_ACTION:
+        return {
+            ...state,
+            ...action.reducedState
+        };
+
+```
+
+## Expected Sample Data
+```
+    stateObj = {
+        fruit: {
+            apple: "",
+            banana: ""
+        },
+        foo: "",
+        bar: "",
+        pizza: {
+            cheese: "",
+            pepperoni: ""
+        }
+    }
+```
+
+## Questions or Improvements?
+Github: https://github.com/wfknowles/
+Email: will@willknowles.com
+
+## Contribution
+Made with ❤️ by Will Knowles
+©️2021
---
+++ ReactForms.js
@@ -0,0 +1,207 @@
+class ReactForms {
+
+    constructor() {
+  
+      this.debug = true;
+  
+    }
+  
+    value(event) {
+  
+      try {
+  
+        return this.formatData(event);
+  
+      } catch (error) {
+  
+        throw new SyntaxError(`ReactFormDataError: ${error}`);
+  
+      }
+      
+    }
+  
+    formatData(event) {
+   
+      const { name, value, type } = event.target;
+      let formattedData = {};
+  
+      // input has nested data
+      if (name.includes('.')) {
+  
+        // create reversed array of keys
+        const keyArr = name.split('.').reverse();
+  
+        // build object from reversed array
+        keyArr.forEach((currentKey, i) => {
+  
+          if (i === 0) {
+  
+            if (type === 'checkbox') {
+  
+              formattedData = { [ currentKey ]: event.target.checked };
+  
+            } else {
+  
+              formattedData = { [ currentKey ]: value };
+  
+            }
+            
+          } else {
+  
+            formattedData = { [ currentKey ]: formattedData };
+  
+          }
+  
+        });
+  
+        
+      } else {
+  
+        // input has standard? data, simply return the value, possibly should check if its a checkbox
+        formattedData[name] = value; 
+  
+      }
+      
+      return { event, data: formattedData };
+  
+    }
+  
+    
+    reduce(state, formData) {
+  
+      try {
+  
+        return this.reduceFormData(state, formData);
+  
+      } catch (error) {
+  
+        throw new SyntaxError(`ReactFormsError: ${error}`);
+  
+      }
+      
+    }
+  
+    reduceFormData(state, formData) {
+  
+      const { event, data: incomingState } = formData;
+      const keys = event.target.name.split('.');
+  
+      let stateTest = state;
+      let incomingStateTest = incomingState;
+      let reducedState = {};
+  
+      keys.forEach((key, i) => {
+  
+        // if both state and incomingState have data
+        if (stateTest[key] && incomingStateTest[key]) {
+  
+          // if the data doesnt match
+          if (stateTest[key] != incomingStateTest[key]) {
+  
+            // if both states have children...
+            if (stateTest[key][keys[i+1]] && incomingStateTest[key][keys[i+1]]) {
+  
+              const stateParent = stateTest[key];
+              const incomingStateParent = incomingStateTest[key];
+              const parentKey = [keys[i+1]];
+              const stateChild = stateParent[parentKey];
+              const incomingStateChild = incomingStateTest[key][parentKey];
+  
+              const childrenData = this.childrenDataTypeReducer(
+                stateParent,
+                incomingStateParent,
+                parentKey,
+                stateChild,
+                incomingStateChild
+              );
+  
+              reducedState[key] = childrenData;
+  
+            } else {
+              
+              reducedState[key] = this.dataTypeReducer(state[key], incomingState[key]);
+  
+            } // end if both have children
+  
+          } // end if data doesnt match
+  
+        } // end if both have data
+  
+      }); // end forEach
+  
+      // console.log('reducedFormData', reducedState);
+  
+      return reducedState;
+    }
+  
+    dataTypeReducer(state, incomingState) {
+      const stateType = typeof state;
+      const incomingStateType = typeof incomingState;
+      let reducedDataByType = {};
+  
+  
+      if (stateType === incomingStateType) {
+  
+        if (stateType === 'object') {
+  
+          reducedDataByType = {...state, ...incomingState};
+  
+        } else if (stateType === 'string') {
+  
+          reducedDataByType = incomingState;
+  
+        }
+        else {
+  
+          console.log('EXCEPTION!', {
+            1:{state, incomingState},
+            2:{stateType, incomingStateType}
+          });
+  
+          reducedDataByType = {...state, ...incomingState};
+        }
+      } else {
+  
+        console.log('EXCEPTION!', {
+          1:{state, incomingState},
+          2:{stateType, incomingStateType}
+        });
+  
+        // default handling is to spread both states into the same object
+        reducedDataByType = {...state, ...incomingState};
+      }
+  
+      return reducedDataByType;
+    }
+  
+    childrenDataTypeReducer(stateParent, incomingStateParent, parentKey, stateChild, incomingStateChild) {
+  
+      const stateChildType = typeof stateChild;
+      const incomingStateChildType = typeof incomingStateChild;
+      let reducedState = {};
+  
+      // reduce state by child data type
+      if (stateChildType === 'object' && incomingStateChildType === 'object') {
+  
+        const reducedChild = this.dataTypeReducer(stateChild, incomingStateChild);
+        reducedState = this.dataTypeReducer(stateParent, {[parentKey]: reducedChild});
+  
+      } else if (stateChildType === 'string' && incomingStateChildType === 'string') {
+  
+        reducedState = this.dataTypeReducer(stateParent, incomingStateParent);
+  
+      } else { 
+  
+        console.log('EXCEPTION!', {
+          stateParent, incomingStateParent, parentKey, stateChild, incomingStateChild
+        });
+  
+      }
+  
+  
+      return reducedState;
+    }
+  
+  }
+  
+  export default new ReactForms();
